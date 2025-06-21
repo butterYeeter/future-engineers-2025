65531ea260e34c4911585d4e8caa213322f52399
butterYeeter <fajandarraaid@gmail.com>
2025-06-21 17:53:38 +0200

    PID controller
    Attemping to use a PID controller to drive in a straight line using ultra sonic sensor

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor
-from pybricks.parameters import Port, Stop, Direction
-from pybricks.tools import wait
+from pybricks.parameters import Port, Stop, Direction, Button
 from pybricks.nxtdevices import UltrasonicSensor
+from pybricks.iodevices import Ev3devSensor
 from calib import calib_steering
 
 
 ev3 = EV3Brick()
 big_motor = Motor(Port.B, positive_direction = Direction.COUNTERCLOCKWISE)
 med_motor = Motor(Port.C)
-ultrasonic = UltrasonicSensor(Port.S3)
-
-
-target_distance = 150
 calib_steering(med_motor)
 
-Kp = 0.5
-Ki = 0
-Kd = 0
-big_motor
-# big_motor.run(360)
+compass = Ev3devSensor(Port.S1)
+ultson = UltrasonicSensor(Port.S2)
 
-c = med_motor.control
+
+target_value = 200
+Kp = 10
+Ki = 0
+Kd = 0.5
+
+last_error = 0
+error = 0
+derivative = 0
+integral = 0
+
+big_motor.dc(100)
 
 while True:
-  error = target_distance - ultrasonic.distance()
-  med_motor.run_target(540, Kp * error)
-  
+  error = target_value - ultson.distance()
+  integral += error
+  derivative = error - last_error
+  last_error = error
+  correction = Kp * error + Ki * integral + Kd * error
+  # med_motor.run_until_stalled(correction, duty_limit=30)
+  med_motor.run(correction)
+  print(correction, med_motor.angle())
+  buttons = ev3.buttons.pressed()
+  if Button.UP in buttons:
+    med_motor.run_target(360, 0)
+    break
