7247a269d3cc104efd655b67258c648d683992fd
butterYeeter <fajandarraaid@gmail.com>
2025-07-05 15:28:37 +0200

    tests

--- src/ev3/test.py
+++ src/ev3/test.py
@@ -11,16 +11,15 @@ from calib import calib_steering
 
 
 class Action:
-  def __init__(self, robot):
-    self.robot = robot
+  def __init__(self):
     self.done = False
 
   def loop(self):
     pass
 
 class Turn(Action):
-  def __init__(self, robot, direction):
-    super().__init__(robot)
+  def __init__(self, direction):
+    super().__init__()
     self.direction = direction
     self.has_turned = False
     self.cooldown = 250
@@ -45,28 +44,50 @@ u = UltrasonicSensor(Port.S1)
 steer = Motor(Port.C)
 drive = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
 
-initial = g.get_angle()
+t_ang = g.get_angle()
 
 calib_steering(steer)
 
 gains = [float(g) for g in argv[1:]]
 print("Controller gains: ", gains)
 
-target_distance = 300
+target_distance = 150
 pid1 = PIDController(target_distance, gains[0], gains[1], gains[2])
-pid2 = PIDController(initial, 2.2, 0.02, 1.5)
+pid2 = PIDController(t_ang, 2.2, 0.02, 1.5)
 
 
 log = DataLog("target len", "len", "correciton", name="../logs/log", timestamp=False)
 
 watch = StopWatch()
 
+current_action = None
+turning = False
+cool_down = 250
+ld_prev = u.distance()*cos(radians(abs(t_ang - g.get_angle())))
+
 drive.dc(80)
 while True:
   left = u.distance()
-  da = abs(initial - g.get_angle())
-  correction = pid1.loop(left*cos(radians(da)))
-  steer.track_target(-correction)
-  log.log(target_distance, left, -correction)
+  da = abs(t_ang - g.get_angle())
+  correction = 0
+
+
+  if left - ld_prev > 1000 and turning == False:
+    # turning = True
+    cool_down = 100
+    t_ang += 90
+    pid2.set_target(t_ang)
+
+  if turning == True:
+    correction = pid2.loop(g.get_angle())
+    cool_down -= 1
+    if cool_down == 0:
+      turning = False
+  else:
+    correction = -pid1.loop(left)
+
+  steer.track_target(correction)
+  log.log(target_distance, left, correction)
+    
 
   # utime.sleep_ms(200)
