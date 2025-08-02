9953e10928710f2ae13ecf5a452c2b834848c182
butterYeeter <fajandarraaid@gmail.com>
2025-08-02 09:23:56 +0200

    Better DodgeD

--- src/ev3/test.py
+++ src/ev3/test.py
@@ -8,6 +8,7 @@ from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from calib import calib_steering
 from util import is_blue
+from pybricks.tools import DataLog
 
 steer_motor = Motor(Port.C)
 drive_motor = Motor(Port.B)
@@ -28,18 +29,25 @@ area = b["w"] * b["h"]
 print("Area is {}".format(area))
 
 
-
+log = DataLog("", name="../log", timestamp=False)
 
 drive_motor.run(600)
 dodging = False
 change = 30
 counter = 0
+correction = 0
   
 while True:
   block = pixy.get_largest_signiture()
   angle = gyro.get_angle()
 
-  correction = -pid.loop(block["cx"])
+  if block["type"] != 1 and block["type"] != 2:
+    correction = gpid.loop(angle)
+    print("g")
+  else:
+    correction = -pid.loop(block["cx"])
+    print("c")
+
   # correction = gpid.loop(angle)
   steer_motor.track_target(correction)
 
@@ -58,19 +66,19 @@ while True:
   #   gpid.target += 90
   if dodging:
     dodging = False
-    counter = 600
+    counter = 500
     prevtarg = gpid.target
     gpid.target = angle + change
     while counter > 0:
       counter -= 1
-      if counter == 250:
+      if counter == 200:
         gpid.target = prevtarg
       
       correction = gpid.loop(gyro.get_angle())
       steer_motor.track_target(correction)
       
     # drive_motor.brake()
-    break
+    # break
       
 
 
