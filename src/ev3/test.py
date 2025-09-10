c90e2c982bf9e631775a0e7dca640e7396f3638e
butterYeeter <fajandarraaid@gmail.com>
2025-09-10 13:31:11 +0200

    Parallel parking V1

--- src/ev3/test.py
+++ src/ev3/test.py
@@ -4,7 +4,7 @@ from pixy import Pixy
 from color import ColorSensor
 from gyro import Gyro
 from pid import PIDController
-from pybricks.ev3devices import Motor
+from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.iodevices import UARTDevice
 from pybricks.parameters import Port, Stop
 from util import *
@@ -14,7 +14,7 @@ from pybricks.tools import DataLog
 LEFT = 90
 RIGHT = -90
 SPEED = 800
-
+DISTANCE = 200
 
 steer_motor = Motor(Port.D)
 drive_motor = Motor(Port.B)
@@ -22,143 +22,117 @@ drive_motor = Motor(Port.B)
 pid = PIDController(target=316//2, Kp=0.25, Kd=0.05, output_limit=40)
 pixy = Pixy(Port.S2, 0x54)
 
-usb = USB()
 pico_uart = UARTDevice(Port.S4, 115200)
 pico_uart.clear()
 color = ColorSensor(pico_uart, (0.43243244, 0.32432431, 0.21621622))
 gyro = Gyro(pico_uart)
+left_us = UltrasonicSensor(Port.S1)
+right_us = UltrasonicSensor(Port.S3)
 target_angle = gyro.get_angle()
+target_distance = DISTANCE
 gpid = PIDController(target_angle, 2.2, 0.02, 1.5, 32, 5)
+upid = PIDController(target_distance, 0.8, 0.01, 0.7, 35, 100)
+directions = {
+  "l": 1,
+  "r": -1
+}
 
 calib_steering(steer_motor)
-
-b = pixy.get_largest_signiture()
-area = b["w"] * b["h"]
-print("Area is {}".format(area))
 
 
 log = DataLog("", name="../log", timestamp=False)
 
-drive_motor.run(SPEED)
-dodging = False
-dodge_obstacle = 0
-change = 30
-counter = 0
-correction = 0
-previous_obstacle = 0
-lt = False
+# drive_motor.run(SPEED)
+# dodging = False
+# dodge_obstacle = 0
+# change = 30
+# counter = 0
+# precision = 0
+# previous_obstacle = 0
+# lt = False
 
-def l_turn(direction):
-  distance = distance_to_angle(500) + drive_motor.angle()
-  while drive_motor.angle() < distance:
-      correction = gpid.loop(gyro.get_angle())
-      steer_motor.track_target(correction)
+def clamp(val, min_val, max_val):
+  return max(min_val, min(val, max_val))
 
-  drive_motor.run(-SPEED)
-  gpid.target += direction
-  angle = gyro.get_angle()
-  while abs(gpid.target - angle) > 3:
-      angle = gyro.get_angle()
-      correction = -gpid.loop(angle)
-      steer_motor.track_target(correction)
+def sign(val):
+  return val/abs(val)
 
-  drive_motor.run(SPEED)
-    
+def turn_to_angle(angle=90, direction="l", power=50, steering=40, compensation=10, precision=1):
+  """Turns the robot to a specific target angle
+
+  int power - Value from -100 to 100 where positive is forwards
+
+  int steering - Steering motor angle from 1 to 42
+
+  int angle - Angle to which to turn in degrees(>0)
   
-while True:
-  block = pixy.get_largest_signiture()
+  int precision - How close to the target angle the robot needs to be in degrees before ending the turn
+
+  int compensation - How much to compensate in angle to prevent overshooting the turning angle
+
+  str direction - Opiton for turning left or right ("l" or "r", not sensitive, e.g Right, Left, L, R, l, r, Ricardo, Leonardo, etc.)
+  """
+  print("Starting Angle: {}".format(gyro.get_angle()))
+  direction = directions[direction[0].lower()]
+  steer_motor.run_angle(1000, 0, then=Stop.HOLD,wait=True)
+  angle = gyro.get_angle()+((abs(angle)-compensation)*sign(power)*direction)
+  print("Target Angle: {}".format(angle))
+  steering = clamp(abs(steering), 1, 40)*direction
+  print("Steering Angle: {}".format(steering))
+  # steer_motor.run_angle(1000, steering, then=Stop.HOLD,wait=True)
+  steer_motor.track_target(steering)
+  curr = gyro.get_angle()
+
+  drive_motor.dc(power)
+  while abs(curr-angle)>=precision:
+    print("Current Gyro Angle: {}".format(curr))
+    print("Difference: {}".format(abs(curr-angle)))
+    curr = gyro.get_angle()
+  drive_motor.hold()
+
+
+def set_steer(target_deg):
+  steer_motor.track_target(target_deg)
+
+#turn_to_angle(30, "left")
+#turn_to_angle(30, "right", -50)
+
+wall_counter = 0
+upid.target = 270
+drive_motor.dc(60)
+last_dist = right_us.distance()
+last_angle = gyro.get_angle()
+gyro_weight = 0.7
+us_weight = 0.3
+while wall_counter < 4:
+  dist = right_us.distance()
+  delta_dist = abs(dist - last_dist)
+  last_dist = dist
+
   angle = gyro.get_angle()
+  delta_angle = abs(angle - last_angle)
+  last_angle = angle
 
-  if block["type"] != 1 and block["type"] != 2:
-    correction = gpid.loop(angle)
-    # print("g")
+  if delta_angle > 30:
+    us_weight = 0
+    gyro_weight = 1
   else:
-    correction = -pid.loop(block["cx"])
-    # print("c")
+    us_weight = 0.3
+    gyro_weight = 0.7
 
-  # correction = gpid.loop(angle)
-  steer_motor.track_target(correction)
+  if delta_dist > 150:
+    us_weight *= 0.5
+    wall_counter += 1
+    print("Wall")
 
-  area = block["w"] * block["h"]
-  if area > 3000:
-    # drive_motor.brake()
-    if block["type"] == 1:
-      dodging = True
-      change = -35
-    if block["type"] == 2:
-      dodging = True
-      change = 35
+  correction = us_weight * upid.loop(dist) + gyro_weight * gpid.loop(angle)
+  set_steer(correction)
+  print("Dist: {}".format(delta_dist))
+drive_motor.hold()
 
-  # if is_blue(color.get_color_corrected()):
-  #   gpid.target += 90
-  if lt > 0:
-    l_turn(lt)
-    previous_obstacle = 0
-    lt = 0
-  # detected_col = color.get_detected()
-  # c = color.get_color_corrected()
-  # detected_col = get_color(c)
-  # print(detected_col)
-  # if detected_col == ORANGE:
-  #   gpid.target = prevtarg + RIGHT
-  #   print("ORANGEEGEEEE")
-  c = color.get_color_corrected()
-  detected_col = get_color(c)
-  if detected_col == BLUE:
-      print("Turning left...")
-      gpid.target += LEFT
-      # self.color.reset_detected()
-  elif detected_col == ORANGE:
-      print("Turning RIGHT")
-      gpid.target += RIGHT
-
-  if dodging:
-    dodging = False
-    previous_obstacle = block["type"]
-    # 420
-    distance = distance_to_angle(350) + drive_motor.angle()
-    counter = 70
-    prevtarg = gpid.target
-    # gpid.target = angle + change
-    gpid.target += change
-    while drive_motor.angle() < distance:
-      # counter -= 1
-      if counter == 30:
-        gpid.target = prevtarg
-      
-      correction = gpid.loop(gyro.get_angle())
-      steer_motor.track_target(correction)
-
-    distance = distance_to_angle(100) + drive_motor.angle()
-    gpid.target = prevtarg
-    while drive_motor.angle() < distance:
-      # print("TEST")
-      correction = gpid.loop(gyro.get_angle())
-      steer_motor.track_target(correction)
-
-      c = color.get_color_corrected()
-      detected_col = get_color(c)
-      if previous_obstacle == 1 and detected_col == ORANGE:
-        print("CHEESE")
-        lt = RIGHT
-        break
-      # detected_col = color.get_detected()
-      # if detected_col == ORANGE:
-      #   gpid.target = prevtarg + RIGHT
-      #   print("ORANGEEGEEEE")
-      #   break
-
-    # c = color.get_color_corrected()
-    # detected_col = get_color(c)
-    # if previous_obstacle == 1 and detected_col == ORANGE:
-    #   print("CHEESE")
-    #   lt = RIGHT
-    #   break
-
-      
-    
-    # drive_motor.brake()
-    # break
-      
+turn_to_angle(60, "Right", -80)
 
 
+turn_to_angle(60, "left", -80)
+# set_steer(0)
+steer_motor.run_target(1000, 0)
